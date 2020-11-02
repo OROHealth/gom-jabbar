ef6e6667031b35ea535e1bbf751f546bccc75515
Pierre <pseguin2011@protonmail.com>
2020-11-02 19:11:00 +0000

    Outremona Squeezing cheese Added handler for outremona squeezing the cheese and return an http status code of 200 (success).

--- poutine_production/outremona_service/src/main.rs
+++ poutine_production/outremona_service/src/main.rs
@@ -10,15 +10,21 @@ pub const PORT: u16 = 8000;
 /// The Outremona service: take cheese from a box an squeeze it
 #[tokio::main]
 async fn main() {
+    // routes for the outremona service
     let health_route = warp::path!("health").map(|| warp::reply());
-
     let take_cheese_route = warp::path!("take-cheese").and_then(OutremonaHandlers::get_cheese);
+    let squeeze_cheese_route =
+        warp::path!("take-cheese").and_then(OutremonaHandlers::squeeze_cheese);
 
     let cors = warp::cors()
         .allow_any_origin()
         .allow_header("content-type")
         .allow_methods(&[Method::GET, Method::POST, Method::DELETE]);
 
-    let routes = health_route.or(take_cheese_route).with(cors);
+    let routes = health_route
+        .or(take_cheese_route)
+        .or(squeeze_cheese_route)
+        .with(cors);
+
     warp::serve(routes).run(SocketAddr::new(HOST, PORT)).await;
 }
--- poutine_production/outremona_service/src/outremona_handlers.rs
+++ poutine_production/outremona_service/src/outremona_handlers.rs
@@ -3,8 +3,23 @@ use warp::{Rejection, Reply};
 pub struct OutremonaHandlers;
 
 impl OutremonaHandlers {
+    /// Handler for get cheese request, prints that the
+    /// robot is taking cheese
+    ///
+    /// ## Returns
+    /// Http status code 200
     pub async fn get_cheese() -> Result<impl Reply, Rejection> {
         println!("Taking Cheese");
+        Ok(StatusCode::OK)
+    }
+
+    /// Handler for squeeze cheese request, prints that the
+    /// robot is squeezing the cheese
+    ///
+    /// ## Returns
+    /// Http status code 200
+    pub async fn squeeze_cheese() -> Result<impl Reply, Rejection> {
+        println!("Squeezing Cheese");
         Ok(StatusCode::OK)
     }
 }
