7bcea0114d43de260b4a0dbd1aa455f73547b962
jashanjotsingh <s_jashan@encs.concordia.ca>
2020-11-07 10:17:47 -0500

    nordo gets potatoes from verduny and cooks them

--- poutine/lapoutinemtl/services/nordo/handlers.go
+++ poutine/lapoutinemtl/services/nordo/handlers.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/labstack/echo/v4"
+	"io/ioutil"
+	"lapoutinemtl.com/utils"
 	"net/http"
 )
 
 func boil(c echo.Context) error {
-	return c.JSON(http.StatusOK, nil)
+
+	tr := &http.Transport{}
+	client := &http.Client{Transport: tr}
+
+	// Call the api
+	resp, err := client.Get("http://host.docker.internal:5143/verduny/potatoes/get")
+	if err != nil {
+		fmt.Errorf("err is: %s", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	var potatoes utils.Potatoes
+	err = json.Unmarshal(body, &potatoes)
+	if err != nil {}
+	if potatoes.Portion != "" {
+		potatoes.AreCooked = true
+		return c.JSON(http.StatusOK, potatoes)
+	} else {
+		return c.JSON(http.StatusBadRequest, struct {
+			Message string
+		}{"nothing to cook man! grab a portion first."})
+	}
 }
 
