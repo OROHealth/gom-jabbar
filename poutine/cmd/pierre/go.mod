123fbfea79401d42e26fbd584f2f64a886cb72c0
Dany Patrie <dany@orohealth.me>
2020-11-10 11:25:44 -0500

    add pierre cmd

---
+++ poutine/cmd/pierre/go.mod
@@ -0,0 +1,5 @@
+module github.com/dpatrie/gom-jabbar/poutine/cmd/pierre
+
+go 1.15
+
+require github.com/joho/godotenv v1.3.0
---
+++ poutine/cmd/pierre/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/dpatrie/gom-jabbar/poutine/pubsub"
+	"github.com/dpatrie/gom-jabbar/poutine/resto"
+	"github.com/dpatrie/gom-jabbar/poutine/robots"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+	bus, err := pubsub.NewRedisBus(os.Getenv("REDIS_HOST"))
+	if err != nil {
+		log.Fatalf("pierre error: %s", err)
+	}
+	log.Fatal(robots.NewPierre(bus, inventory()).ServeHTTP(os.Getenv("HTTP_HOST")))
+}
+
+func inventory() *resto.Inventory {
+	return &resto.Inventory{
+		AvailableCheeses: map[resto.CheeseKind]uint{
+			resto.CheeseKindCouicCouic: 10000,
+			resto.CheeseKindNotSoGood:  10000,
+		},
+		AvailablePotatoes: map[resto.PotatoKind]uint{
+			resto.RedPotato:    1000,
+			resto.YellowPotato: 1000,
+			resto.SweetPotato:  1000,
+		},
+		AvailableCardbox: 1000,
+	}
+}
