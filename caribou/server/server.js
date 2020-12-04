eec8aca8f7569fae8d47afdb3d42a1b0f37e74f0
Wanda Antoine <wanda@thinkful.com>
2020-12-04 09:06:30 -0500

    refactored files for later testing

--- caribou/server/server.js
+++ caribou/server/server.js
@@ -1,6 +1,5 @@
 const app = require('./app')
-
-const PORT = process.env.PORT || 8000
+const { PORT } = require('./config')
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
