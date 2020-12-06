542b02dfaf04f5948fff2d4794ee7868cec0c0fd
Corentin DESFARGES <corentin.desfarges@gmail.com>
2020-12-06 15:47:29 +0100

    feat(map): center map on current user location

--- caribou/front/src/components/Map/index.js
+++ caribou/front/src/components/Map/index.js
@@ -1,15 +1,37 @@
-import {MapContainer, TileLayer} from "react-leaflet";
+import {useEffect} from 'react';
+import {MapContainer, TileLayer, useMap} from "react-leaflet";
+import {usePosition} from 'use-position';
 
 import 'leaflet/dist/leaflet.css';
 import './styles.css';
 
+const DEFAULT_ZOOM = 13.5;
+const DEFAULT_POSITION = {
+  lat: 48.5908666,
+  lng: 7.7245304
+};
+
+const UserLocator = () => {
+  const map = useMap();
+  const {latitude, longitude} = usePosition();
+
+  useEffect(() => {
+    if (latitude && longitude) {
+      map.flyTo([latitude, longitude], DEFAULT_ZOOM, {animate: false});
+    }
+  }, [latitude, longitude, map]);
+
+  return null
+};
+
 const Map = () => {
   return (
-    <MapContainer center={{lat: 48.5533, lng: 7.75}} zoom={13.5}>
+    <MapContainer center={DEFAULT_POSITION} zoom={DEFAULT_ZOOM}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+      <UserLocator/>
     </MapContainer>
   );
 };
