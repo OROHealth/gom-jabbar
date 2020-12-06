46bb4fd79ab791826179135eda1b67bf5c05f567
Corentin DESFARGES <corentin.desfarges@gmail.com>
2020-12-06 13:08:24 +0100

    feat(init): initialize front-end project

---
+++ caribou/front/README.md
@@ -0,0 +1,9 @@
+# CaribouCo
+
+## Installation
+
+Un petit coup de `yarn install`, puis un `yarn start`, et vous accéderez à "CaribouCo" !
+
+## Fonctionnel
+
+Un super logo qui respire. Voilà c'est tout.
---
+++ caribou/front/src/App.js
@@ -0,0 +1,14 @@
+import logo from './caribou.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
