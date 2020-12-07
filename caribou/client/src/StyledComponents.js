94c1f9e3466c8fd9ec085887196182d2fb6505fb
Matcote <matthewcotesipod@gmail.com>
2020-12-06 19:36:00 -0500

    styled components component to keep things DRY

---
+++ caribou/client/src/StyledComponents.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  align-self: flex-end;
+  width: auto;
+  background: #8c7851;
+  outline: none;
+  color: white;
+  padding: 10px 15px;
+  font-weight: bold;
+  font-size: large;
+  border: 3px solid #020826;
+  border-radius: 20px;
+  cursor: pointer;
+`;
+export const Input = styled.input`
+  border: none;
+  border-bottom: 3px solid #020826;
+  width: auto;
+  font-size: large;
+  padding: 8px;
+  outline: none;
+`;
+export const Title = styled.div`
+  font-size: xx-large;
+  font-weight: bold;
+  margin: 30px 0 40px 20px;
+`;
