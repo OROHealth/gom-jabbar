ce03563d9b23010ca5f730a46ea9747c945f7aa8
Matcote <matthewcotesipod@gmail.com>
2020-12-07 20:13:02 -0500

    markers on map + info when markers are clicked

--- caribou/client/src/pages/Map.js
+++ caribou/client/src/pages/Map.js
@@ -1,6 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  InfoWindow,
+  LoadScript,
+  Marker,
+} from "@react-google-maps/api";
+import jb from "../assets/jb.png";
 
 const containerStyle = {
   width: "350px",
@@ -12,20 +18,31 @@ const options = {
   disableDefaultUI: true,
   zoomControl: true,
 };
+
 const Map = () => {
   const [humans, setHumans] = useState([]);
+  const [selected, setSelected] = useState(null);
   const [center, setCenter] = useState({
+    //default center is montreal
     lat: 45.5017,
     lng: -73.5673,
   });
+
   const handleMapClick = (event) => {
     console.log(event);
+    console.log(humans);
   };
+  useEffect(() => {
+    fetch("/humans")
+      .then((res) => res.json())
+      .then((data) => setHumans(data.data));
+  }, []);
   return (
     <Wrapper>
       <LoadScript
         googleMapsApiKey={process.env.REACT_APP_MAPS_KEY}
         onLoad={() => {
+          //if user accepts location services, map recenters on user's location
           navigator.geolocation.getCurrentPosition(function (position) {
             setCenter({
               lat: position.coords.latitude,
@@ -43,10 +60,26 @@ const Map = () => {
         >
           {humans.map((human) => (
             <Marker
-              key={human.time.toISOString()}
-              position={{ lat: human.lat, lng: human.lng }}
+              key={human._id}
+              position={human.position}
+              icon={{
+                url: jb,
+                scaledSize: new window.google.maps.Size(30, 35),
+              }}
+              onClick={() => setSelected(human)}
             />
           ))}
+          {selected ? (
+            <InfoWindow
+              position={selected.position}
+              onCloseClick={() => setSelected(null)}
+            >
+              <Info>
+                <h2>Trashy Human!</h2>
+                <p>Trash level: {selected.trashingLevel}</p>
+              </Info>
+            </InfoWindow>
+          ) : null}
         </GoogleMap>
       </LoadScript>
     </Wrapper>
@@ -58,5 +91,11 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+const Info = styled.div`
+  h2 {
+    font-weight: bold;
+    margin-bottom: 4px;
+  }
 `;
 export default Map;
