53c9c24fde74fea92d5e530a5008029dda161b6e
Thomas Cantinelli <tcantinelli@gmail.com>
2020-12-09 15:48:16 +0100

    BACK ADD resetDB Service

--- schitts/back/src/services/resetDB.ts
+++ schitts/back/src/services/resetDB.ts
@@ -1,2 +1,18 @@
-import dropAllCollections from '../testSetup';
+import { dropAllCollections } from '../testSetup';
+import addConstantsDatas from './addConstantsDatas';
 
+const resetDB = async () => {
+	try {
+		//Drop collections
+		await dropAllCollections();
+
+		//Add constants datas
+		await addConstantsDatas();
+
+		return true;
+
+	} catch (_error) {
+		return false
+	}
+
+}
