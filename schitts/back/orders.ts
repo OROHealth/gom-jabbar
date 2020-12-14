3501dbebbd0c28913af979598075ed1000db9fa8
tcantinelli <tcantinelli@gmail.com>
2020-12-14 16:43:22 +0100

    BACK Start ordres with feedback

---
+++ schitts/back/orders.ts
@@ -0,0 +1,48 @@
+import { IBills } from "../models/bills";
+import { ICustomers } from "../models/customers";
+import { IMenus } from "../models/menus";
+import { Orders, IOrders } from "../models/orders";
+import { ITones } from "../models/tones";
+
+const OrdersController = {
+	async getAll(): Promise<IOrders[]> {
+		const orders = await Orders.find({});
+		return orders;
+	},
+
+	async count(): Promise<number> {
+		const count = await Orders.estimatedDocumentCount();
+		return count;
+	},
+	
+	async findBy(body: {[key: string]: any}): Promise<IOrders[]> {
+		const orders = Orders.find(body);
+		return orders;
+		},
+
+	async add(customer: ICustomers, bill: IBills, orderDate: Date, menu: IMenus, tone: ITones): Promise<IOrders | null> {
+		const newOrder = new Orders({
+			customer,
+			bill,
+			orderDate,
+			menu,
+			tone
+		});
+		const orderSaved = await newOrder.save({});
+		if (orderSaved) return orderSaved;
+		return null;
+	},
+
+	async patchFeedback(orderID: string, feedback: number): Promise<IOrders | null> {
+		const patchedOrder = await Orders.findByIdAndUpdate(
+			orderID,
+			{feedback},
+			{ new: true }
+		);
+
+		if (patchedOrder) return patchedOrder;
+		return null;
+	}
+}
+
+export default OrdersController;
