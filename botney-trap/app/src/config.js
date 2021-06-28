c9c9469955cde92a3679d694fc1e654019e05772
Jhoan Franco <jhoan.franco@correounivalle.edu.co>
2021-06-28 14:18:24 -0500

    feat: add kustomize configuration and add app src

---
+++ botney-trap/app/src/config.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const envalid = require("envalid");
+
+const envApp = envalid.cleanEnv(
+  process.env,
+  {
+    ENV: envalid.str({
+      desc: "Environment",
+      default: "production",
+      devDefault: "development",
+    }),
+    CLOUD: envalid.str({
+      desc: "Cloud",
+      default: "local",
+      devDefault: "local",
+    }),
+  },
+  {
+    dotEnvPath: null,
+  }
+);
+
+exports.default = {
+  CLOUD: envApp.CLOUD,
+  ENV: envApp.CLOUD,
+};
