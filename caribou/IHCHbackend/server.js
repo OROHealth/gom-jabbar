9e61ccaafd5149bd686c6e023d7032d22e707f67
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2021-11-28 22:37:38 -0500

    basic human signaling with lat and long

--- caribou/IHCHbackend/server.js
+++ caribou/IHCHbackend/server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-
-
 const server = express();
 
 server.use(express.json());
@@ -8,6 +6,16 @@ server.use(express.json());
 
 server.get('/api/test',(req,res)=>{
   res.send({response:"Hellow World"});
+});
+
+
+//basic signaling api takes in longitude and latitude as well as trashing level on a scale from 0-10
+server.put('/api/signalHuman',(req,res)=>{
+    let lat = req.query.lat;
+    let long = req.query.long;
+    let trashingLevel = req.query.trashingLevel
+    res.send({response:"you sucessfully reported a human at: "+lat+" "+long,
+    trashingLevelRes:"You reported a trashing level of:"+trashingLevel});
 });
 
 
