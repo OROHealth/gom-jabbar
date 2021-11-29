0c0027107df03bc203d379071edf15580ce6ad81
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2021-11-28 22:44:43 -0500

    some boiler plate

--- caribou/IHCHbackend/server.js
+++ caribou/IHCHbackend/server.js
@@ -8,6 +8,11 @@ server.get('/api/test',(req,res)=>{
   res.send({response:"Hellow World"});
 });
 
+//request presence of human in radius around a specific latitude and longitude
+server.get('/api/requestPresence',(req,res)=>{
+    //TODO
+})
+
 
 //basic signaling api takes in longitude and latitude as well as trashing level on a scale from 0-10
 server.put('/api/signalHuman',(req,res)=>{
@@ -16,6 +21,11 @@ server.put('/api/signalHuman',(req,res)=>{
     let trashingLevel = req.query.trashingLevel
     res.send({response:"you sucessfully reported a human at: "+lat+" "+long,
     trashingLevelRes:"You reported a trashing level of:"+trashingLevel});
+});
+
+//allows the user to signal that it is ready to antler-exchange
+server.put('/api/signalAntlerExchange',(req,res)=>{
+    //TODO 
 });
 
 
