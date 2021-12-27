e0ace13e48085ee37b42ce246af428ee904b33e1
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2021-12-27 18:33:33 -0500

    log position on click

--- caribou/frontend/src/components/Map/Map.js
+++ caribou/frontend/src/components/Map/Map.js
@@ -1,14 +1,22 @@
 import GoogleMapReact from "google-map-react";
 import "../Map/Map.css"
 import IHCHicon from "../../img/IHCH.png"
-import { process_params } from "express/lib/router";
+
 const Map = ({ center, zoom }) => {
+
+  const apiLoaded = (map,maps) => {
+    map.addListener("click", (mapsMouseEven)=>{
+      console.log(JSON.stringify(mapsMouseEven.latLng.toJSON()));
+    })
+  };
   return (
     <div className="map">
             <GoogleMapReact
                 bootstrapURLKeys={{ key:process.env.REACT_APP_GOOGLE_API_KEY }}
                 defaultCenter={ center }
                 defaultZoom={ zoom }
+                yesIWantToUseGoogleMapApiInternals
+                onGoogleApiLoaded={({ map, maps }) => apiLoaded(map, maps)}
             >
               <img className="icon" src= {IHCHicon} lat="45.50391" lng="-73.5575758"></img>
             </GoogleMapReact>
@@ -20,6 +28,8 @@ Map.defaultProps = {
   center: { lat: 45.5017, lng: -73.5673 },
   zoom: 12
 }
+
+
   
 
 export default Map;
