a940693d60f4ada2e2bfc4b5a5cfc8b4a42d7a5d
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2021-12-26 23:26:08 -0500

    nav bar

--- caribou/frontend/src/App.js
+++ caribou/frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./styles.css";
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <h1 className="lorem">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </h1>
     </div>
   );
 }
---
+++ caribou/frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Navbar.css";
+import { motion } from "framer-motion";
+import "../../styles.css";
+import caribou from "../../img/caribou.svg"
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <img src = {caribou} className="mainIcon" alt="Caribou icon"/>
+      <motion.button
+        whileHover={{
+          scale: 1.1,
+          boxShadow: "0px 0px 4px rgb(255,255,255)",
+          color: "#4787c7",
+          border: "1px solid #4787c7",
+        }}
+      >
+        Sign in
+      </motion.button>
+
+      <motion.button
+        whileHover={{
+          scale: 1.1,
+          boxShadow: "0px 0px 4px rgb(255,255,255)",
+          color: "#4787c7",
+          border: "1px solid #4787c7",
+        }}
+      >
+        Sign in
+      </motion.button>
+
+      <motion.button
+        whileHover={{
+          scale: 1.1,
+          boxShadow: "0px 0px 4px rgb(255,255,255)",
+          color: "#4787c7",
+          border: "1px solid #4787c7",
+        }}
+      >
+        Sign in
+      </motion.button>
+
+      <motion.button
+        whileHover={{
+          scale: 1.1,
+          boxShadow: "0px 0px 4px rgb(255,255,255)",
+          color: "#4787c7",
+          border: "1px solid #4787c7",
+        }}
+      >
+        Sign in
+      </motion.button>
+
+      <motion.button
+        whileHover={{
+          scale: 1.1,
+          boxShadow: "0px 0px 4px rgb(255,255,255)",
+          color: "#4787c7",
+          border: "1px solid #4787c7",
+        }}
+      >
+        Sign in
+      </motion.button>
+    </div>
+  );
+};
+
+export default NavBar;
