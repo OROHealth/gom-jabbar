1c76c1c2ee40bb2e179cb72867167044ad7e18cc
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2021-12-31 01:27:49 -0500

    implement muugrm encryption

--- caribou/backend/src/routes/message.js
+++ caribou/backend/src/routes/message.js
@@ -2,6 +2,21 @@ const express = require("express");
 const pool = require("../db/db");
 const router = express.Router();
 
+function muugrm(message){
+    var superSecureMessage = "";
+    for (var i =0; i<message.length;i++){
+        if(isVowel(message[i])){
+            superSecureMessage+= "muu";
+        }
+        else superSecureMessage+= "grm";
+    }
+    return superSecureMessage;
+}
+
+function isVowel(char){
+    return 'aeiou'.includes(char);
+}
+
 router.route('/').get(async (req,res)=>{
     //get list of messages(if no param load last 10 and load more otherwise)
     try{
@@ -15,10 +30,12 @@ router.route('/').get(async (req,res)=>{
     }
 }).post(async (req,res)=>{
     try{
-        const query=`INSERT INTO message (inbox_id, sender_id, reciever_id, message) VALUES ($1, $2, $3, $4);`
-        const values=[req.query.inbox_id, req.query.sender_id,req.query.reciever_id,req.query.message];
+
+        const message = muugrm(JSON.stringify(req.body.message));
+        const query=`INSERT INTO message (inbox_id, sender_id, reciever_id, message) VALUES ($1, $2, $3, $4);`;
+        const values=[req.body.inbox_id, req.body.sender_id,req.body.reciever_id,message];
         await pool.query(query, values);
-        res.json("Successfully created message");
+        res.json(`Successfully created message:${message}`);
 
     }
     catch(err){
